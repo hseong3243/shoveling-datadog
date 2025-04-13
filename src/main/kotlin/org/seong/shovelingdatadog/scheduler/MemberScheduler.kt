884d9993f7d1286886ea23e140b6167eadab6efb
hseong3243 <hseong3243@gmail.com>
2025-04-13 17:07:16 +0900

    테스트용 회원 스케줄러 수정

--- src/main/kotlin/org/seong/shovelingdatadog/scheduler/MemberScheduler.kt
+++ src/main/kotlin/org/seong/shovelingdatadog/scheduler/MemberScheduler.kt
@@ -3,20 +3,22 @@ package org.seong.shovelingdatadog.scheduler
 import org.seong.shovelingdatadog.service.AcceptMemberMessage
 import org.seong.shovelingdatadog.service.MemberService
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
+@EnableScheduling
 class MemberScheduler(
     private val memberService: MemberService,
     private val rabbitTemplate: RabbitTemplate
 ) {
-    @Scheduled(cron = "0/10 * * * * *")
+    @Scheduled(cron = "0 * * * * *")
     fun accept() {
         this.memberService.findAllJoinedMember()
             .forEach {
                 rabbitTemplate.convertAndSend(
-                    "accept-member", AcceptMemberMessage(it.memberId)
+                    "accept-member", "", AcceptMemberMessage(it.memberId)
                 )
             }
     }
